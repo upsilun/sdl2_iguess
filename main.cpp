79b8e292466f16c495b42eb7bb2d465f0ba30fb0
Yazeed Alahmad <72664813+upsilun@users.noreply.github.com>
2024-04-13 08:28:48 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -16,6 +16,9 @@ SDL_Rect playerRect = { SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_SIZE, PLAYER
 SDL_Rect objects[NUM_OBJECTS];
 SDL_Rect obstacles[NUM_OBSTACLES];
 
+int score = 0;
+int playerSpeed = 5;
+
 void initializeSDL() {
     SDL_Init(SDL_INIT_VIDEO);
     gWindow = SDL_CreateWindow("Simple SDL Game", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
@@ -38,16 +41,20 @@ void handleInput() {
         else if (e.type == SDL_KEYDOWN) {
             switch (e.key.keysym.sym) {
                 case SDLK_UP:
-                    playerRect.y -= 5;
+                    if (playerRect.y > 0)
+                        playerRect.y -= playerSpeed;
                     break;
                 case SDLK_DOWN:
-                    playerRect.y += 5;
+                    if (playerRect.y < SCREEN_HEIGHT - PLAYER_SIZE)
+                        playerRect.y += playerSpeed;
                     break;
                 case SDLK_LEFT:
-                    playerRect.x -= 5;
+                    if (playerRect.x > 0)
+                        playerRect.x -= playerSpeed;
                     break;
                 case SDLK_RIGHT:
-                    playerRect.x += 5;
+                    if (playerRect.x < SCREEN_WIDTH - PLAYER_SIZE)
+                        playerRect.x += playerSpeed;
                     break;
             }
         }
@@ -61,7 +68,7 @@ void generateObjectsAndObstacles() {
         objects[i].w = OBJECT_SIZE;
         objects[i].h = OBJECT_SIZE;
     }
-    
+
     for (int i = 0; i < NUM_OBSTACLES; ++i) {
         obstacles[i].x = rand() % (SCREEN_WIDTH - OBSTACLE_SIZE);
         obstacles[i].y = rand() % (SCREEN_HEIGHT - OBSTACLE_SIZE);
@@ -77,55 +84,56 @@ bool checkCollision(SDL_Rect rect1, SDL_Rect rect2) {
 void render() {
     SDL_SetRenderDrawColor(gRenderer, 255, 255, 255, 255);
     SDL_RenderClear(gRenderer);
-    
+
     // Render player
     SDL_SetRenderDrawColor(gRenderer, 255, 0, 0, 255);
     SDL_RenderFillRect(gRenderer, &playerRect);
-    
+
     // Render objects
     SDL_SetRenderDrawColor(gRenderer, 0, 255, 0, 255);
     for (int i = 0; i < NUM_OBJECTS; ++i) {
         SDL_RenderFillRect(gRenderer, &objects[i]);
     }
-    
+
     // Render obstacles
     SDL_SetRenderDrawColor(gRenderer, 0, 0, 255, 255);
     for (int i = 0; i < NUM_OBSTACLES; ++i) {
         SDL_RenderFillRect(gRenderer, &obstacles[i]);
     }
-    
+
     SDL_RenderPresent(gRenderer);
 }
 
 int main(int argc, char* args[]) {
     initializeSDL();
     generateObjectsAndObstacles();
-    
+
     bool quit = false;
     while (!quit) {
         handleInput();
-        
+
         // Collision detection
         for (int i = 0; i < NUM_OBJECTS; ++i) {
             if (checkCollision(playerRect, objects[i])) {
                 std::cout << "Object collected!\n";
                 objects[i].x = rand() % (SCREEN_WIDTH - OBJECT_SIZE);
                 objects[i].y = rand() % (SCREEN_HEIGHT - OBJECT_SIZE);
-
+                score++;
+                playerSpeed += 1; // Increase speed
                 std::cout << '\a';
             }
         }
-        
+
         for (int i = 0; i < NUM_OBSTACLES; ++i) {
             if (checkCollision(playerRect, obstacles[i])) {
                 std::cout << "Game Over!\n";
                 quit = true;
             }
         }
-        
+
         render();
     }
-    
+
     closeSDL();
     return 0;
 }
